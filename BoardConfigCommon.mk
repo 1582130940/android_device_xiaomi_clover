a62517282e0cf091b3655445c8a45a84bb1cc256
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:07:49 +0100

    sdm660-common: BoardConfig: Enable vendor RIL service
    * We use qcrild
    
    Change-Id: Iaab09d72110fdc3887fda6f543b988bdb3e98a4c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,6 +118,9 @@ TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
