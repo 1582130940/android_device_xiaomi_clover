a1ee40824f4505ada543d69f4e0ee4c33c779083
sbwml <984419930@qq.com>
2022-07-19 15:49:21 +0800

    clover: Build vendor.display.config@2.0
    * dlopen failed: library "vendor.display.config@2.0.so" not found

--- device.mk
+++ device.mk
@@ -196,7 +196,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.config@1.1
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
