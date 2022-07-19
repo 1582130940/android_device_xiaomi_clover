a3135f640dd8214b6406e1c7d72dd39c1d167d0e
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-20 01:38:53 +0800

    clover: Set PRODUCT_AAPT_PREF_CONFIG
    https://developer.android.com/training/multiscreen/screendensities

--- device.mk
+++ device.mk
@@ -392,6 +392,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
