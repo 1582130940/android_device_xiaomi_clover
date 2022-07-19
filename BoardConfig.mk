dbae12c95a5f588a9c7f07221a27b2eb05d3c935
Michael Bestas <mkbestas@lineageos.org>
2022-07-19 11:58:01 +0800

    clover: Enable TARGET_HW_DISK_ENCRYPTION

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,6 +57,9 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
