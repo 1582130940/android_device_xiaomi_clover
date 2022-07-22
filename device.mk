78565399f5787c0cc9891e51ea01af91d38641b7
ThatisJigen <gianfranco.liguori.96@gmail.com>
2022-07-22 12:00:12 +0800

    clover: Add mediaswcodec-seccomp policy
    * mediaswcodec: Could not read additional policy file
    '/vendor/etc/seccomp_policy/mediaswcodec.policy'

--- device.mk
+++ device.mk
@@ -442,7 +442,8 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy \
+    $(LOCAL_PATH)/seccomp/mediaswcodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
