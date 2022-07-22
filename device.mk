b6a4f7d387e30d1f88a3d8a898cf69a3425348f8
1582130940 <19632674+1582130940@users.noreply.github.com>
2022-07-22 14:22:39 +0800

    clover: Update Sensor from LA.UM.8.2.r1-06700-sdm660.0
    * sensor_def_qcomdev from V10.3.2.0.ODJCNXM
    * init.qcom.sensors from V12.5.7.0.QFGCNXM

--- device.mk
+++ device.mk
@@ -404,6 +404,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
 # Shims
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
