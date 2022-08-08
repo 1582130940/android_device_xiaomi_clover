b8fc566640ee57f7479bbe612a436f9a3174507e
Bruno Martins <bgcngm@gmail.com>
2022-08-08 20:15:50 +0800

    sdm660-common: device: Copy cgroups and task_profiles directly from system/core
    This ensures that the most up-to-date JSON files are shipped.
    
    Change-Id: Ic9fae188784825ae6dc607c13b403e37290b7067

--- sdm660.mk
+++ sdm660.mk
@@ -150,6 +150,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 endif
 
+# Control groups and task profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    system/core/libprocessgroup/profiles/task_profiles_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
