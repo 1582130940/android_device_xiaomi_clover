e15db051abb1459e603576af540b046444adeab8
Edwin Wong <edwinwong@google.com>
2022-08-08 19:56:05 +0800

    sdm660-common: device: Clearkey plugin to drm HAL v1.4
    Test: atest
    atest
    CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I106a727f2f29bf5472484402b2801a630b0d6301

--- sdm660.mk
+++ sdm660.mk
@@ -181,7 +181,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
