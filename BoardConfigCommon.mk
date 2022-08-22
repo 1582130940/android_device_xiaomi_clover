aa5ce2482ccd569ee0c7f8cfcea20964473fd3be
Bruno Martins <bgcngm@gmail.com>
2022-08-22 11:41:31 +0800

    sdm660-common: BoardConfig: Drop default DRM service
    This is not needed, since Widevine DRM and Clearkey DRM
    services are available.
    
    Change-Id: I97023f1bab88ddb8fa4cb205df190c516a759951

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,9 +64,6 @@ TARGET_USES_HWC2 := true
 TARGET_USES_GRALLOC1 := true
 TARGET_USES_ION := true
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
