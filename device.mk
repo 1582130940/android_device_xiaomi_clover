3da1ad0a1614461cb4d062d56153779cf7a8f1ff
Jakub Pawlowski <jpawlowski@google.com>
2023-01-04 11:00:02 +0800

    sdm660-common: device: Use bluetooth.audio@2.1
    bluetooth.audio@2.1 adds new functionalities required for Bluetooth LE
    Audio development
    
    Bug: 150670922
    Change-Id: Iac2ece0c029a38fef82e81f5ba3ba491163818ef

--- device.mk
+++ device.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.a2dp@1.0-impl \
     android.hardware.bluetooth.a2dp@1.0-service \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor
 
