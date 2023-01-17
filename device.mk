17453c536d120754c302ed972341d47809249505
Sujin Panicker <spanic@codeaurora.org>
2023-01-17 15:32:27 +0800

    clover: device: Enable soundtrigger hidl hal 2.3
    Enable soundtrigger hidl hal 2.3 for SP with
    kernel version 4.19.

--- device.mk
+++ device.mk
@@ -83,7 +83,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.3-impl \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
