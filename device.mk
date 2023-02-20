7b62106a84f75064ce780a9015bfe8ab0d5d0c86
PIPIPIG233666 <pig.priv@gmail.com>
2023-02-20 15:43:50 +0800

    clover: device: Place a copy of audio platform info intcodec as audio platform info
    Silence following:
    * D platform_info: platform_info_init: Failed to open
    audio_platform_info.xml, using defaults.
    
    Change-Id: Icf9bb063d1bef72143c01009da17727f5b6fdefd

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
